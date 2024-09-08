b7ac6c747b79ea56a540d8bae104b99392300123
odom50 <kbibi0865@gmail.com>
2024-09-08 10:54:54 +0900

    Fixed MongoDB connection issue and updated options

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 var app = express();
 app.use(cors());
 
+
+
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 
